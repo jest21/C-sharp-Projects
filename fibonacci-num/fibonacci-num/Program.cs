31a66e1e5c7fe9c7b8f99f162f339ac9bec7b584
Jestina <jestinajohn30@gmail.com>
2016-05-02 17:29:12 +0530

    Added Fibonacci-Num

---
+++ fibonacci-num/fibonacci-num/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci_num
+{
+    class Program
+    {
+        public static int fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter the value of n:");
+            n = int.Parse(Console.ReadLine());
+            for(int i=0;i<n;i++)
+                
+            Console.WriteLine(fibonacci(i));
+            Console.ReadLine();
+        }
+    }
+}
