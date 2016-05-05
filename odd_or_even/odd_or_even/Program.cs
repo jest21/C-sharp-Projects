e50b7b551f8a344ec01623fc5898fc1500b925f1
Jestina <jestinajohn30@gmail.com>
2016-05-05 21:21:58 +0530

    Added Odd or Even Program

---
+++ odd_or_even/odd_or_even/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_or_even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("enter a number:");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("the number is even ");
+                
+            }
+            else
+            {
+                Console.WriteLine("the number is odd ");
+               
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
