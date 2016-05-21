35c4711ba0406c73f5e85060135ebbf1f2786f2c
Jestina <jestinajohn30@gmail.com>
2016-05-21 22:57:26 +0530

    FactorialRecursionFun

---
+++ factorial_recursionfun/factorial_recursionfun/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial_recursionfun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number ");
+            int number = int.Parse(Console.ReadLine());
+            long fact = GetFactorial(number);
+            Console.WriteLine("{0} Factorial is {1}",number,fact);
+            Console.ReadKey();
+        }
+        private static long GetFactorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            return number * GetFactorial(number - 1);
+        }
+    }
+}
