272cc8c3d0fa556b030b70f81fb5552b163ef420
Jestina <jestinajohn30@gmail.com>
2016-05-23 00:53:36 +0530

    Added another Project

---
+++ usinglinq/usinglinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usinglinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 50, 32, 85, 14, 51, 96, 700, 65, 100, 300, 79, 55, 400 };
+            var program = from num in numbers where num < 50 select num;
+            Console.WriteLine("Numbers less than 50 are :");
+            foreach (int i in program)
+            {
+                Console.Write(i + "\t");
+            }
+            var prgram = from num in numbers where num > 50 select num;
+            Console.WriteLine("\nNumbers more than 50 are :");
+            foreach (int i in prgram)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
