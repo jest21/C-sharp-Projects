f8a6ca8b7382d4a79d5e82a14d44943a8d3c1f4a
Jestina <jestinajohn30@gmail.com>
2016-08-15 16:36:21 +0530

    Added Linq

---
+++ linq_pgm/linq_pgm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_pgm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Marks = new int[6];// { 1, 2, 8, 74, 58, 0 };
+            int i;
+            Console.WriteLine("\nenter a array of elements ");
+            for (i = 0; i < 6; ++i)
+            {
+                Marks[i] = int.Parse(Console.ReadLine());
+            }
+
+            var maxres = (from m in Marks select m).Max();
+            var minres = (from n in Marks select n).Min();
+            Console.WriteLine("\nLargest value is " + maxres);
+            Console.WriteLine("\nSmallest value is " + minres);
+            Console.ReadLine();
+
+        }
+    }
+}
