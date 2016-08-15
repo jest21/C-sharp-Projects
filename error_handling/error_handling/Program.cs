f21f3a39bb85ae8611397ceb14c427439c03d20e
Jestina <jestinajohn30@gmail.com>
2016-08-15 16:36:09 +0530

    Added Error Handling

---
+++ error_handling/error_handling/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace error_handling
+{
+    class Program
+    {
+
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Enter a & b:");
+         int a = int.Parse(Console.ReadLine());
+         int b = int.Parse(Console.ReadLine());
+         try
+         {
+            if(b == 0)
+            {
+               throw (new ZeroException("Zero found"));
+            }
+            else
+            {
+               Console.WriteLine("Result: {0}", a/b);
+            }
+         }
+         catch(ZeroException e)
+         {
+            Console.WriteLine("ZeroException: {0}", e.Message);
+         }
+         Console.ReadKey();
+      }
+   }
+}
+
+public class ZeroException: Exception
+{
+   public ZeroException(string message): base(message)
+   {
+
+   }
+}
+        
+    
+
